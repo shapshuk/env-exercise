020212d9eb961ba656edae0e9a92efce1a2dd2ca
shapshuk <andrewshapshuk@gmail.com>
2024-08-05 17:03:52 +0300

    updated greeting screen

--- app/src/main/java/com/shapshuk/env_exercise/MainActivity.kt
+++ app/src/main/java/com/shapshuk/env_exercise/MainActivity.kt
@@ -4,24 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shapshuk.env_exercise.ui.theme.EnvexerciseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val buildTypeName = BuildConfig.BUILD_TYPE
+
         enableEdgeToEdge()
         setContent {
             EnvexerciseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = buildTypeName,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,10 +41,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = modifier
+                .align(Alignment.CenterHorizontally),
+            text = "Hello! It's $name!",
+        )
+        Image(
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .align(Alignment.CenterHorizontally),
+            painter = painterResource(id = R.drawable.app_logo),
+            contentDescription = null,
+        )
+    }
 }
 
 @Preview(showBackground = true)
